6fca77c75a1d3a4dffa29e8e70b6d38ab10df306
manrajc13 <manrajc1010@gmail.com>
2026-04-08 18:39:14 +0530

    error logs

--- server/src/lib/utils/email.js
+++ server/src/lib/utils/email.js
@@ -27,7 +27,9 @@ dns.setDefaultResultOrder('ipv4first');
 import nodemailer from 'nodemailer';
 
 export const sendOTPEmail = async (email, otp) => {
-    const transporter = nodemailer.createTransport({
+
+    try{
+        const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
         secure: false,
@@ -36,16 +38,20 @@ export const sendOTPEmail = async (email, otp) => {
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD
         }
-    });
+        });
 
-    await transporter.sendMail({
-        from: process.env.EMAIL,
-        to: email,
-        subject: "Email Verification OTP",
-        html: `
-            <h2>Your verification OTP</h2>
-            <h1>${otp}</h1>
-            <p>This OTP expires in 10 minutes.</p>
-        `
-    });
+        await transporter.sendMail({
+            from: process.env.EMAIL,
+            to: email,
+            subject: "Email Verification OTP",
+            html: `
+                <h2>Your verification OTP</h2>
+                <h1>${otp}</h1>
+                <p>This OTP expires in 10 minutes.</p>
+            `
+        });
+    } catch (error) {
+        console.log("Error sending OTP email: ", error);
+        throw new Error("Failed to send OTP email");
+    }
 };
